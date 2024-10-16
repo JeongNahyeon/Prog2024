3cd13ec97fe7eed5faa3a9ae30d349af2170c268
JeongNahyeon <skgus0031@gmail.com>
2024-10-16 13:59:40 +0900

    Add files via upload

---
+++ 034_2cha won/034_2cha won.cpp
@@ -0,0 +1,49 @@
+﻿// 034_2cha won.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[4][5] = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, {11, 12, 13, 14, 15 }, {16, 17, 18, 19, 20} };
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%5d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    int b[2][3] = { { 2 ,4 ,6 }, { 8, 6, 3 } };
+    int c[2][3] = { 1,2,3,4,5,6 };
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf(" %3d ", b[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf(" %3d ", c[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    int t[2][3][4] = { {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}},
+        {{13,14,15,16}, {17,18,19,20}, {21,22,23,24}} };
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++)
+                printf(" %4d ", t[i][j][k]);
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
+
---
+++ 037_strlen(s)/037_strlen(s).cpp
@@ -0,0 +1,48 @@
+﻿
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[100] = "hello";// 배열을 안쓰면 5가아니라 6이라고 출력됨
+    int len = strlen(s);
+
+    printf("배열의 칸수 : %d\n", (int)sizeof(s));
+    printf("문자열의 길이 : %d\n", len);
+
+    char s1[] = "hello";
+    char s2[100];
+
+    strcpy_s(s2, s1);
+    printf("s1 : %s\n", s1);
+    printf("s2 : %s\n", s2);
+
+    // 문자열 상수를 복사할수있다.
+    strcpy_s(s2, "Konyang University");
+    printf("s1 : %s\n", s1);
+    printf("s2 : %s\n", s2);
+
+    char greeting[10] = "Hello, ";
+    char name[100];
+
+    printf("이름 입력 : ");
+    scanf_s("%s", name, sizeof(name));
+
+    int x = sizeof(greeting) - strlen(greeting) - 1;
+    strncat_s(greeting, name, x);
+    //strcat_s(greeting, "!");
+
+    printf("%s", greeting);
+
+    //주의할점1 문자배열은 반드시 '\0'로 끝나있어야한다.
+    char str[10] = "";
+    strcat_s(str, "Hello");
+    printf("%s\n", str);
+
+    char str1[] = "sample";
+    char str2[] = "simple";
+
+    printf("%d\n", strcmp(str1, str2));
+
+
+}
